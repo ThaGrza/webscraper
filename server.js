f98bb6b82127f9cc6aefb4bbf4e14445666d514d
Andrew Griswold <andrewgriswold7@gmail.com>
2019-07-25 23:38:19 -0500

    Working api routes

--- server.js
+++ server.js
@@ -21,14 +21,53 @@ app.use(express.urlencoded({ extened: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Connects to Mongo DB.
 mongoose.connect("mongodb://localhost/webScraper", { useNewUrlParser: true });
 
+
 // API Routes.
-app.get("https://news.developer.nvidia.com/").then(function(res){
-    let $ = cheerio.load(res.data);
-    $("article a").each(function(i, element) {
+app.get("/", function(req, res){
+    res.render("index");
+});
+
+app.get("/scrape" , function(req, res) {
+    axios.get("https://news.developer.nvidia.com/").then(function(response){
+    let $ = cheerio.load(response.data);
+    $("article h1").each(function(i, element) {
         let result = {};
+        result.title = $(this)
+            .children("a")
+            .text();
+        result.link = $(this)
+            .children("a")
+            .attr("href");
         console.log(result);
+        // res.json(result);
+
+    //     db.Article.create(result).then(function(dbArticle) {
+    //         console.log(dbArticle);
+    //     })
+    //     .catch(function(err) {
+    //         console.log(err);
+    //     });
     });
+    // res.send("Scrape Comeplete");
+    });
+});
+
+
+// Starts server.
+app.listen(PORT, function() {
+    console.log( `
+    *******************************
+
+    App running on PORT ` + PORT + `
+
+    *******************************
+    `);
 });

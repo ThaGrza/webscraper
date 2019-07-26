d9d8c01de1194062687e1fd5168374928f0c9ab7
Andrew Griswold <andrewgriswold7@gmail.com>
2019-07-25 23:57:30 -0500

    api routes

--- server.js
+++ server.js
@@ -46,19 +46,54 @@ app.get("/scrape" , function(req, res) {
         result.link = $(this)
             .children("a")
             .attr("href");
-        console.log(result);
-        // res.json(result);
+        // console.log(result);
 
-    //     db.Article.create(result).then(function(dbArticle) {
-    //         console.log(dbArticle);
-    //     })
-    //     .catch(function(err) {
-    //         console.log(err);
-    //     });
+        db.Article.create(result).then(function(dbArticle) {
+            console.log(dbArticle);
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
     });
-    // res.send("Scrape Comeplete");
+    res.send("Got' em");
     });
 });
+
+app.get("/articles", function(req, res) {
+    db.Article.find({})
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+app.get("/articles/:id", function(req, res) {
+    db.Article.findOne({ _id: req.params.id })
+      .populate("note")
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+  app.post("/articles/:id", function(req, res) {
+    db.Note.create(req.body)
+      .then(function(dbNote) {
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      })
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+
 
 
 // Starts server.
